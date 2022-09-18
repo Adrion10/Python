ba04db804e02f9b245cc1fb6b1b7a85972e90e0d
Adrion10 <lacehilaQgmail.com>
2022-09-18 17:37:16 +0200

    Debugging Practise

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 
-
+#String Manipulation and Code Intelligence
 print("Hello World!\nHello World\nHello World")
 print("Hello" + " Marisola")
 print("Hello " + "Marisola")
 print("Hello" + " " + "Marisola")
+
+
+#Debugging Practise
+print("Day 1 - String Manipulation\nString Concatenation is done with the '+' sign.\ne.g. print('Hello ' + 'world')\nNew lines can be created with a backslash and n.")
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
