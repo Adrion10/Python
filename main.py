140e7a253f52e3e142429470f034746552b51c2c
Adrion10 <lacehilaQgmail.com>
2022-09-18 17:13:51 +0200

    String Manipulation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print(' what to print') ")
+print("Hello World!\nHello World\nHello World")
+print("Hello" + " Marisola")
+print("Hello " + "Marisola")
+print("Hello" + " " + "Marisola")
